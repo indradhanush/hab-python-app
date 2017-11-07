5f02e22807fd61a395b5fe4895a48efc0b3cd551
Indradhanush Gupta <indra@kinvolk.io>
2017-11-07 19:25:44 +0530

    Add /

--- app/urls.py
+++ app/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from quotes.views import IndexView
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^', IndexView.as_view()),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request):
+        return HttpResponse("Hello, World!")
+
+
+
