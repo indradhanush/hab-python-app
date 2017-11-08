6589bbe722950728cdbbd2e438410548ba6c8602
Indradhanush Gupta <indra@kinvolk.io>
2017-11-08 06:07:46 +0000

    Clone repo in build

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -4,30 +4,38 @@ pkg_version="0.1.0"
 pkg_maintainer="Indradhanush Gupta <indra@kinvolk.io>"
 pkg_license=('Python-2.0')
 pkg_source="https://github.com/indradhanush/${pkg_name}/archive/v${pkg_version}.tar.gz"
-pkg_deps=( core/python )
+pkg_deps=(
+    core/python
+    core/git
+    core/make
+)
+demo_pkg_dir="$HAB_CACHE_SRC_PATH/$pkg_name-$pkg_version"
 # pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="348e82e816855fc323af9cbecc1ba8027b21c91298a22c2575450bae554f8ed8"
-# pkg_deps=(core/glibc)
-# pkg_build_deps=(core/make core/gcc)
-# pkg_lib_dirs=(lib)
-# pkg_include_dirs=(include)
-# pkg_bin_dirs=(bin)
-# pkg_pconfig_dirs=(lib/pconfig)
-# pkg_svc_run="bin/haproxy -f $pkg_svc_config_path/haproxy.conf"
-# pkg_exports=(
-#   [host]=srv.address
-#   [port]=srv.port
-#   [ssl-port]=srv.ssl.port
-# )
-# pkg_exposes=(port ssl-port)
-# pkg_binds=(
-#   [database]="port host"
-# )
-# pkg_binds_optional=(
-#   [storage]="port host"
-# )
-# pkg_interpreters=(bin/bash)
-# pkg_svc_user="hab"
-# pkg_svc_group="$pkg_svc_user"
-# pkg_description="Some description."
-# pkg_upstream_url="http://example.com/project-name"
+# pkg_shasum="348e82e816855fc323af9cbecc1ba8027b21c91298a22c2575450bae554f8ed8"
+
+do_download() {
+    return 0
+}
+
+do_verify() {
+    return 0
+}
+
+do_unpack() {
+    mkdir -p "$demo_pkg_dir/src"
+    pushd "$demo_pkg_dir/src"
+    git clone https://github.com/$pkg_origin/$pkg_name
+    popd
+    return 0
+}
+
+do_build() {
+    pushd "$demo_pkg_dir/src/$pkg_name"
+    make
+    popd
+    return 0
+}
+
+do_install() {
+    return 0
+}
